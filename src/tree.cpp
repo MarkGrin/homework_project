6f9c73b46a745f58628c4ea0e6b11e593e421b9b
Mark Grin <markgrin@localhost.localdomain>
2017-11-19 20:48:07 +0300

    *

--- src/tree.cpp
+++ src/tree.cpp
@@ -23,13 +23,13 @@ void ExprNode::write (std::string& dot, std::size_t& node)
     dot += "node" + std::to_string(nodeNumber) + " [label=\"" + data_ + "\"];\n";
     if ( left_ )
     {
-        dot += "node" + std::to_string(nodeNumber) + " -> node" + std::to_string(node) + ";\n";
         left_->write(dot, node);
+        dot += "node" + std::to_string(nodeNumber) + " -> node" + std::to_string(node) + ";\n";
     }
     if ( right_ )
     {
-        dot += "node" + std::to_string(nodeNumber) + " -> node" + std::to_string(node) + ";\n";
         right_->write(dot, node);
+        dot += "node" + std::to_string(nodeNumber) + " -> node" + std::to_string(node) + ";\n";
     }
 
 }    
