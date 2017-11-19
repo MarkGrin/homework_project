c252568aa16d112c90a20d9fc6bce3b298486bfa
Mark Grin <markgrin@localhost.localdomain>
2017-11-19 20:50:12 +0300

    *

--- src/tree.cpp
+++ src/tree.cpp
@@ -18,18 +18,20 @@ ExprNode::~ExprNode ()
 
 void ExprNode::write (std::string& dot, std::size_t& node)
 {
-    std::size_t nodeNumber = node;
+    std::size_t rootNode = node;
     node++;
-    dot += "node" + std::to_string(nodeNumber) + " [label=\"" + data_ + "\"];\n";
+    dot += "node" + std::to_string(rootNode) + " [label=\"" + data_ + "\"];\n";
     if ( left_ )
     {
+        std::size_t childNode = node;
         left_->write(dot, node);
-        dot += "node" + std::to_string(nodeNumber) + " -> node" + std::to_string(node) + ";\n";
+        dot += "node" + std::to_string(rootNode) + " -> node" + std::to_string(childNode) + ";\n";
     }
     if ( right_ )
     {
+        std::size_t childNode = node;
         right_->write(dot, node);
-        dot += "node" + std::to_string(nodeNumber) + " -> node" + std::to_string(node) + ";\n";
+        dot += "node" + std::to_string(rootNode) + " -> node" + std::to_string(childNode) + ";\n";
     }
 
 }    
